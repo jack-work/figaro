567973f399687fa9e85a087cb9b558e888880b96
gluck <gluck@kelliher.info>
2026-09-16 02:21:20 -0400

    chore: figaro 0.37.3 (flake version)

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
       overlays.default = final: prev: {
         figaro = final.buildGoModule rec {
           pname = "figaro";
-          version = "0.37.2";
+          version = "0.37.3";
           src = self;
           vendorHash = "sha256-sTOoAKFZmXAoZlTIOyUpoyZIRjxS697JhlbjU+ctflo=";
 
